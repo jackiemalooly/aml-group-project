56f8ddeb107900d8e828266b1b5b9b8f80f65c7d
jackiemalooly <jackie.malooly@gmail.com>
2025-03-16 15:13:19 +0000

    Update training data and run training script

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import comet_ml
 import os
 import requests
+import glob
 
 from utils import (
 AverageMeter, 
@@ -22,17 +23,20 @@ log = Logger()
 log.open("logs/logfile.txt")
 
 # Initialize comet_ml
-COMET_ML_API_KEY = os.getenv("COMET_ML_API_KEY")
-if COMET_ML_API_KEY is None:
-    raise ValueError("COMET_ML_API_KEY is not set.")
-comet_ml.init(project_name="your-project-name", api_key="COMET_ML_API_KEY")
+#COMET_ML_API_KEY = os.getenv("COMET_ML_API_KEY")
+#if COMET_ML_API_KEY is None:
+#    raise ValueError("COMET_ML_API_KEY is not set.")
+#comet_ml.init(project_name="your-project-name", api_key="COMET_ML_API_KEY")
 
-model = YOLO("yolo11n.pt")
-device = "cuda"
-model = model.to(device)
+model = YOLO(args.model_name)
 
-def train(model, dataset, epochs=10, imgsz=640):
-    results = model.train(data=dataset, epochs=epochs, imgsz=imgsz)
+def train(model=str, dataset_path=None, epochs=10, imgsz=640):
+    yaml_files = glob.glob(os.path.join(dataset_path, '*.yaml'))
+    if not yaml_files:
+      raise FileNotFoundError(f"No YAML files found in {dataset_path}")
+    yaml_file_path = yaml_files[0]
+    print(f"Using YAML file: {yaml_file_path}")
+    results = model.train(data=yaml_file_path, epochs=epochs, imgsz=imgsz)
     log.write(f"Results: {results}")
     return results
 
@@ -45,7 +49,7 @@ def test(model, dataset):
 def main():
     # Main function to handle training or testing
     if args.model_mode == "train":
-        train(args.model_name, args.dataset_location, epochs=args.epochs, imgsz=args.imgsz)
+        train(model_name=args.model_name, dataset_path=args.dataset_location, epochs=args.epochs, imgsz=args.imgsz)
     elif args.model_mode == "test":
         test(args.model_name, args.dataset_location)
     else:
