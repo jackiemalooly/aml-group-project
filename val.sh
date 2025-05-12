8e78cf49f9f448e46c7a8bc426bc789f88397683
Rishiraj Sengupta <58643955+rishieee@users.noreply.github.com>
2025-05-12 18:49:03 +0100

    Update val.sh

--- val.sh
+++ val.sh
@@ -1,6 +1,6 @@
 python3 main.py \
 --model_mode val \
---model_name runs/detect/train12/weights/best.pt \
+--model_name runs/detect/train12/weights/best.pt \ #update this 
 --epochs 5 \
 --batch 16 \
 --imgsz 640 \
